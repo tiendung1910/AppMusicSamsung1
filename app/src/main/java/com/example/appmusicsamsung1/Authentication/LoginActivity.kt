92911b61b72a6cb1284f874a97ba239b3f26f708
buiduchieu <duchieubui25012002@gmail.com>
2022-05-16 16:41:57 +0700

    login and register with firebase

--- app/src/main/java/com/example/appmusicsamsung1/Authentication/LoginActivity.kt
+++ app/src/main/java/com/example/appmusicsamsung1/Authentication/LoginActivity.kt
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.example.appmusicsamsung1.MainActivity
 import com.example.appmusicsamsung1.R
 import com.example.appmusicsamsung1.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
@@ -16,8 +19,33 @@ class LoginActivity : AppCompatActivity() {
         Toast.makeText(this,"Welcome to Login",Toast.LENGTH_LONG).show()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //handle login
+        val Login = binding.LoginBtn
+        Login.setOnClickListener {
+            val email = binding.LoginEmail.text.toString()
+            val password = binding.LoginPassword.text.toString()
+            handleLogin(email,password)
+        }
+        //redirect to register form
         binding.RegisterRedirect.setOnClickListener{
             startActivity(Intent(this,RegisterActivity::class.java))
+        }
+    }
+
+    private fun handleLogin(email:String, password: String){
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Vui lòng nhập đầy đủ thông tin",Toast.LENGTH_LONG).show()
+            return
+        }else{
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+                    Toast.makeText(this,"Đăng nhập thành công",Toast.LENGTH_LONG).show()
+                    startActivity(Intent(this,MainActivity::class.java))
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this,"Vui lòng kiểm tra lại thông tin đăng nhập",Toast.LENGTH_LONG).show()
+                }
         }
     }
 }
--- app/src/main/java/com/example/appmusicsamsung1/Authentication/RegisterActivity.kt
+++ app/src/main/java/com/example/appmusicsamsung1/Authentication/RegisterActivity.kt
@@ -3,21 +3,54 @@ package com.example.appmusicsamsung1.Authentication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
+import com.example.appmusicsamsung1.MainActivity
 import com.example.appmusicsamsung1.R
 import com.example.appmusicsamsung1.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var binding : ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Toast.makeText(this,"Welcome to register",Toast.LENGTH_LONG).show()
-
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //handle register
+        val Register = binding.RegisterBtn
+        Register.setOnClickListener {
+            val email = binding.RegisterEmail.text.toString()
+            val password = binding.RegisterPassword.text.toString()
+            val repassword = binding.RegisterRepeatPassword.text.toString()
+            handleRegister(email, password, repassword)
+        }
+        //redirect to Login
         binding.LoginRedirect.setOnClickListener {
             startActivity(Intent(this,LoginActivity::class.java))
+        }
+    }
+
+    private fun handleRegister(email:String, password:String, rpassword:String){
+        if(email.isEmpty() || password.isEmpty() || rpassword.isEmpty()){
+            Toast.makeText(this,"Vui lòng điền đầy đủ thông tin",Toast.LENGTH_LONG).show()
+            return
+        }else{
+            if(password == rpassword){
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if(!it.isSuccessful) return@addOnCompleteListener
+                        Log.d("register","${it.result.user?.uid}")
+                        startActivity(Intent(this,MainActivity::class.java))
+                    }
+                    .addOnFailureListener {
+                        Log.d("register", "Đăng ký thất bại")
+                    }
+            }else{
+                Toast.makeText(this,"Mật khẩu phải giống nhau",Toast.LENGTH_LONG).show()
+                return
+            }
         }
     }
 }
